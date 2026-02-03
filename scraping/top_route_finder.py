9ac251b939a480e85a2429949f1a8836ab040d05
orion <orionmarini@gmail.com>
2026-02-03 15:43:14 -0800

    - Deprecated

--- scraping/top_route_finder.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def find_top_routes():
-    route_finder = pd.read_csv('route-finder.csv')
-    routes = route_finder.head(100)
-    print(routes)
-
-if __name__ == '__main__':
-    find_top_routes()
